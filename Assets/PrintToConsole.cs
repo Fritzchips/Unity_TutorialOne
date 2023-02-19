7b46f8d0793207f9ec8000db883d9d23c320dcca
DESKTOP-N5R9FT2\Fritz <fritzjohn_solibio@yahoo.ca>
2023-02-18 23:45:10 -0800

    Tutorial complete

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("We are starting the journey, we better jump this...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
